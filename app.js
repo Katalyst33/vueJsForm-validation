d0e566e4536cb847dc1996033182cd8daa85e66c
hibrid <samuelnmeje@gmail.com>
2019-11-01 01:27:17 +0100

    added github username verification

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 Vue.use(vuelidate.default);
 
 
+const pizzaOrBurger = value => value === 'pizza' || value === 'burger' || !validators.helpers.req(value);
 
+const oldEnoughAndAlive = validators.between(12, 120);
 
 new Vue({
     el: '#app',
@@ -10,88 +12,117 @@ new Vue({
         return {
             form: {
                 name: '',
-                email:'',
-                age:'',
-                password:'',
-                repeatPassword: '',
+                email: '',
+                age: '',
+                food: '',
+                newsletter: '',
+                githubUsername: ''
 
             }
         }
     },
 
-    validations:{
+    validations: {
 
-            form:{
-                name:{
+        form: {
+            name: {
 
-                    required: validators.required,
-                    minLength:validators.minLength(5)
+                required: validators.required,
+                minLength: validators.minLength(5)
 
-                },
-                age:{
+            },
+            age: {
 
-                    required: validators.required,
-                    integer:validators.integer,
-                    between: validators.between(12, 120),
+                required: validators.required,
+                integer: validators.integer,
+                oldEnoughAndAlive
 
-                },
+            },
 
-                email:{
-                    email:validators.email
-                },
+            email: {
+                email: validators.email,
+                required: validators.required(function () {
+                    return !!this.form.newsletter
+                })
+            },
 
-                password:{
-                    required:validators.required,
-                    minLength:validators.minLength(8)
+            githubUsername: {
+                exist(value) {
+                    if (!validators.helpers.req(value)) {
+                        return true
+                    }
 
-                },
-
-             /*   repeatPassword: {
-                    sameAsPassword: sameAs('password')
+                   return axios.get(`//api.github.com/users/${value}`)
                 }
+
+
+            },
+
+            food: {
+                pizzaOrBurger
+            }
+
+
+
+            /*        password: {
+                        required: validators.required,
+                        minLength: validators.minLength(8)
+
+                    },*/
+
+            /*   repeatPassword: {
+                   sameAsPassword: sameAs('password')
+               }
 */
 
-            }
+        }
 
     },
 
-   /* computed: {
-     /!*   emailIsValid() {
-            return !!this.form.email;
+    /* computed: {
+      /!*   emailIsValid() {
+             return !!this.form.email;
 
-        },*!/
+         },*!/
 
-        nameIsValid() {
-            return !!this.form.name;
+         nameIsValid() {
+             return !!this.form.name;
+         },
+
+         ageIsValid() {
+             return typeof this.form.age === 'number' && this.form.age > 12 && this.form.age < 120;
+         },
+
+         formIsValid() {
+             return this.nameIsValid && this.ageIsValid
+         },
+     },*/
+
+    methods: {
+
+        shouldAppendValidClass(field) {
+            return !field.$invalid && field.$model && field.$dirty
         },
 
-        ageIsValid() {
-            return typeof this.form.age === 'number' && this.form.age > 12 && this.form.age < 120;
+        shouldAppendErrorClass(field) {
+            return field.$error
         },
 
-        formIsValid() {
-            return this.nameIsValid && this.ageIsValid
-        },
-    },*/
 
-        methods: {
+        submitForm() {
 
-            submitForm() {
-
-                this.$v.form.$touch();
+            this.$v.form.$touch();
 
 
+            if (!this.$v.form.$invalid) {
+                console.log(`i delivered the message`, this.form)
 
-
-                if (!this.$v.form.$invalid) {
-                    console.log(`i delivered the message`, this.form)
-
-                } else {
-                    console.log(`Invalid Form`)
-
-                }
+            } else {
+                console.log(`Invalid Form`)
 
             }
-        }
 
-    });
+        }
+    }
+
+});
